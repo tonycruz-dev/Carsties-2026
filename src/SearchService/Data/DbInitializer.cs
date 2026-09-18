c6baab1b37ad6bcaef260e8ef87b950c22f86c1a
Antonio Lopes da Cruz <tonycruz1@hotmail.com>
2026-09-18 13:26:09 +0100

    Add SearchService project and implement auction search functionality
    - Added SearchService project with necessary configurations and dependencies.
    - Implemented auction search endpoints and data models.
    - Created auction data initializer and integrated with Meilisearch.
    - Added sample auction data in JSON format.
    - Updated HTTP client for fetching auction data.
    - Enhanced application settings for local development.

---
+++ src/SearchService/Data/DbInitializer.cs
@@ -0,0 +1,44 @@
+﻿using Meilisearch;
+using SearchService.Services;
+namespace SearchService.Data;
+
+public class DbInitializer
+{
+	private const string IndexUid = "items";
+
+	public static async Task ConfigureIndex(WebApplication app)
+	{
+		var client = app.Services.GetRequiredService<MeilisearchClient>();
+		var index = client.Index(IndexUid);
+
+		var settingsTask = await index.UpdateSettingsAsync(new Settings
+		{
+			SearchableAttributes = ["make", "model", "description"],
+			FilterableAttributes = ["seller", "winner", "status", "auctionEnd"],
+			SortableAttributes = ["auctionEnd", "currentHighBid", "createdAt", "updatedAt", "make", "model"]
+		});
+		await client.WaitForTaskAsync(settingsTask.TaskUid);
+	}
+
+	public static async Task FetchMissingAuctions(WebApplication app)
+	{
+		var client = app.Services.GetRequiredService<MeilisearchClient>();
+		var index = client.Index(IndexUid);
+
+		using var scope = app.Services.CreateScope();
+		var auctionSvc = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+
+		var items = await auctionSvc.GetItemsForSearch();
+
+		if (items.Count == 0)
+		{
+			Console.WriteLine("Search index is up to date with the AuctionService");
+			return;
+		}
+
+		var addTask = await index.AddDocumentsAsync(items, primaryKey: "id");
+		await client.WaitForTaskAsync(addTask.TaskUid);
+
+		Console.WriteLine($"Search index populated with {items.Count} items");
+	}
+}
